74cbdc02b1596de8d81317ad122a753e707938fd
muhsin <muhsin.ma.mohamed@gmail.com>
2022-08-22 02:54:22 +0530

    small changes

--- portfolio/src/styles/palette.js
+++ portfolio/src/styles/palette.js
@@ -1,9 +1,7 @@
-
-
-class CustomPalette {
-    static background = "#0D0221";
-    static primary = "#0F084B";
-    static secondary = "#0F084B";
-    static accent1 = "#A6CFD5";
-    static accent2 = "#C2E7D9";
+export default class CustomPalette {
+  static background = "#0D0221";
+  static primary = "#0F084B";
+  static secondary = "#0F084B";
+  static accent1 = "#A6CFD5";
+  static accent2 = "#C2E7D9";
 }
